fcce5727e6989cbf1c23aaee8fcc3a9d9d7a3a03
a3ylf <alexandre3ylf@gmail.com>
2024-03-25 18:57:06 -0300

    os valores de hello world foram gerados!

---
+++ helloWorld_v4/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if "hello world!" == "hello world!" {
+    supermegahelloworldinrunesbecauseitsextremelyimportantthatisinwritteninrunes := ""
+    for _, word := range "hello world!"{
+      supermegahelloworldinrunesbecauseitsextremelyimportantthatisinwritteninrunes = supermegahelloworldinrunesbecauseitsextremelyimportantthatisinwritteninrunes + fmt.Sprintf("" +    fmt.Sprintf(""+fmt.Sprintf(" %d",int(word))))
+    }
+    os.WriteFile("hello.world",[]byte(supermegahelloworldinrunesbecauseitsextremelyimportantthatisinwritteninrunes),0644) 
+
+	} else {
+		fmt.Println("hello world!") 
+	}
+}
